fdac36b3513c674f4999a2f5fff7c09bca924dcd
Zhurbey Artem <zhurbeyartem@gmail.com>
2023-11-17 11:59:24 +0200

    fix

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
-const API_KEY = "live_deerNM7noPj6zACx3z0EyUGms7Rh6oLkSQ5wOhx28BBMDzVVjndjf2BPmEagjo08"
+const API_KEY =
+  'live_deerNM7noPj6zACx3z0EyUGms7Rh6oLkSQ5wOhx28BBMDzVVjndjf2BPmEagjo08';
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 export function fetchBreeds() {
- return axios('https://api.thecatapi.com/v1/breeds')
+  return axios('https://api.thecatapi.com/v1/breeds');
 }
 
 export function fetchCatByBreed(breedId) {
-     return axios(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+  return axios(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  );
 }
-
